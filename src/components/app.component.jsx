040c123c3f7348dca8fd06ddd04e2f223f60bfa6
ashique bin <ananda.rcbu@gmail.com>
2021-08-16 15:06:32 +0600

    increment decrementn

---
+++ src/components/app.component.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Navbar from './navbar.component';
+// import Counter from './counter.component';
+import Counters from './counters.component';
+
+class App extends Component {
+    state = {  }
+
+    render() { 
+        return ( 
+            <>
+                <Navbar/>
+
+                <Counters/>
+            </>
+         );
+    }
+}
+ 
+export default App;
