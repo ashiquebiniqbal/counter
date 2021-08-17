9013120e65f706c1b02dba26c271c55f05f3ca5c
ashique bin <ananda.rcbu@gmail.com>
2021-08-17 20:56:51 +0600

    reset button

---
+++ src/components/counter.component.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  formatCount = () => {
+    if (this.props.value == 0) return "Zero";
+    return this.props.value;
+  };
+
+  render() {
+    return (
+      <>
+        <ul>
+          <li class="list-unstyled">
+            <div>
+              <span class="badge bg-primary">{this.formatCount()}</span>
+              <button
+                onClick={() => this.props.onIncrement(this.props.id)}
+                type="button"
+                class="btn btn-secondary m-2"
+              >
+                +
+              </button>
+              <button
+                onClick={() => this.props.onDecrement(this.props.id)}
+                type="button"
+                class="btn btn-secondary m-2"
+              >
+                -
+              </button>
+              <button type="button" class="btn btn-danger m-2">
+                Delete
+              </button>
+            </div>
+          </li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default Counter;
--- src/components/counters.component.jsx
+++ src/components/counters.component.jsx
@@ -1,25 +1,66 @@
-import React, { Component } from 'react';
-// import Counters from './counters.component';
-import Counter from './counter.components';
-class Counters extends Component {
-    state = { 
-        // counters: []
-        counters: [
-            { id: 0, value: 0 },
-            { id: 1, value: 0 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 }
-        ]
-     }
+import React, { Component } from "react";
+import Counter from "./counter.component";
 
-    render() {
-        return (
-           this.state.counters.map(counter => {
-               return <Counter key={counter.id} />
-           })
-        );
-    }
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 0, value: 0 },
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleReset = () => {
+    const allCounterRest = this.state.counters.map((counter, index) => {
+      const obj = { id: index, value: 0 };
+      return obj;
+    });
+    this.setState({ counters: allCounterRest });
+  };
+
+  handleIncrement = (id) => {
+    const incrementCounters = this.state.counters.map((counter, index) => {
+      if (counter.id == id) return { id: index, value: counter.value + 1 };
+      else return counter;
+    });
+    this.setState({ counters: incrementCounters });
+  };
+
+  handleDecrement = (id) => {
+    const decrementCounters = this.state.counters.map((counter, index) => {
+      if (counter.id == id && counter.value > 0)
+        return { id: index, value: counter.value - 1 };
+      else return counter;
+    });
+    this.setState({ counters: decrementCounters });
+  };
+
+  render() {
+    return (
+      <>
+        <button
+          onClick={this.handleReset}
+          type="button"
+          class="btn btn-warning m-2"
+        >
+          Reset
+        </button>
+        {this.state.counters.map((counters) => {
+          return (
+            <Counter
+              key={counters.id}
+              value={counters.value}
+              id={counters.id}
+              onIncrement={this.handleIncrement}
+              onDecrement={this.handleDecrement}
+            />
+          );
+        })}
+      </>
+    );
+  }
 }
- 
+
 export default Counters;
